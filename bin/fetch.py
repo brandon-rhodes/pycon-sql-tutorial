79ed8666a4b6b4a20a2509abcc24d072cf55b8ca
Brandon Rhodes <brandon@rhodesmill.org>
2012-02-23 01:13:51 -0500

    Am trying a switch to the IMDB as my data set - they still provide flat files!

--- bin/fetch.py
+++ bin/fetch.py
@@ -5,8 +5,16 @@ import shutil
 from urllib2 import urlopen
 
 base = 'http://www2.census.gov/econ/susb/data'
+ftp = 'ftp://ftp.fu-berlin.de/pub/misc/movies/database/'
 
 if __name__ == '__main__':
+    for filename in 'actors.list.gz', 'actresses.list.gz', 'movies.list.gz':
+        shutil.copyfileobj(
+            urlopen(ftp + filename),
+            open(filename, 'w'),
+            )
+
+if False:
     os.chdir(os.path.dirname(__file__))  # /bin directory
     os.chdir('..')                       # main directory
     if not os.path.isdir('cache'):
