d8d5efeeb6587c5174323c5a4e9867954c012140
Brandon Rhodes <brandon@rhodesmill.org>
2012-02-25 15:19:11 -0500

    New routine to parse IMDB data. But why do I run out of disk creating indexes?

--- bin/build.py
+++ bin/build.py
@@ -1,19 +1,10 @@
 #!/usr/bin/env python
 
 import gzip
-import shutil
-import os
 import sqlite3
-from csv import DictReader
 
-if __name__ == '__main__':
-    db = sqlite3.connect('movie.db')
-
-    db.execute('''
-CREATE TABLE actors_and_titles (actor TEXT, title TEXT);
-''')
-
-    lines = iter(gzip.open('actors.list.gz'))
+def import_actors(filename):
+    lines = iter(gzip.open(filename))
     while not next(lines).startswith('Name'):
         continue
     next(lines)   # skip the title underlines
@@ -28,8 +19,44 @@ CREATE TABLE actors_and_titles (actor TEXT, title TEXT);
             actor = maybe_actor
         bi = title.find('[')
         if bi != -1:
+            bj = title.find(']')
+            role = title[bi + 1:bj - 1]
             title = title[:bi]
+        else:
+            role = ''
         title = title.strip()
-        #print (u'%s|%s' % (actor,title)).encode('utf-8')
-        db.execute('INSERT INTO actors_and_titles VALUES'
-                   ' (?, ?)', (actor, title))
+        if '{' in title:  # TV episode
+            continue
+        db.execute('INSERT INTO actor_title_role VALUES'
+                   ' (?, ?, ?)', (actor, title, role))
+    db.commit()
+
+if __name__ == '__main__':
+    db = sqlite3.connect('movie.db')
+    db.execute('''
+CREATE TABLE actor_title_role (actor TEXT, title TEXT, role TEXT);
+''')
+    import_actors('actors.list.gz')
+    import_actors('actresses.list.gz')
+#    db.execute('''
+    ('''
+
+CREATE TABLE movie (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT);
+CREATE TABLE actor (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT);
+CREATE TABLE role (movie_id INTEGER, actor_id INTEGER, role TEXT);
+
+INSERT INTO movie (title) SELECT title FROM actor_title_role;
+INSERT INTO actor (name) SELECT actor FROM actor_title_role;
+
+CREATE INDEX movie_id ON movie (id);
+CREATE INDEX movie_title ON movie (title);
+
+CREATE INDEX actor_id ON actor (id);
+CREATE INDEX actor_name ON actor (name);
+
+INSERT INTO role (movie_id, actor_id, role)
+  SELECT movie.id, actor.id, role FROM actor_title_role
+   JOIN movie USING (title)
+   JOIN actor ON (actor.name = actor_title_role.actor);
+
+''')
