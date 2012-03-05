bd8222d207c5f7a189259b6a82bd083cfa9e74e8
Brandon Rhodes <brandon@rhodesmill.org>
2012-03-05 00:25:39 -0500

    Added a sample SQLAlchemy script for the database, and a few slides to show.

---
+++ alchemy.py
@@ -0,0 +1,60 @@
+from sqlalchemy import create_engine, Column, ForeignKey
+from sqlalchemy import Integer, String, Boolean
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Movie(Base):
+    __tablename__ = 'movie'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    year = Column(Integer)
+    nth = Column(String)
+    for_video = Column(Boolean)
+
+    def __repr__(self):
+        return '<movie %r%s (%s)%s>' % (
+            self.title,
+            (' (%s)' % self.nth if self.nth else ''),
+            self.year,
+            (' (V)' if self.for_video else '')
+            )
+
+class Actor(Base):
+    __tablename__ = 'actor'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    gender = Column(String)
+
+    def __repr__(self):
+        suffix = 'or' if self.gender == 'm' else 'ress'
+        return '<act%s %r>' % (suffix, self.name)
+
+class Role(Base):
+    __tablename__ = 'role'
+
+    movie_id = Column(Integer, ForeignKey('movie.id'),
+                      primary_key=True)
+    actor_id = Column(Integer, ForeignKey('actor.id'),
+                      primary_key=True)
+    name = Column(String, primary_key=True)
+
+    movie = relationship('Movie', backref=backref('roles'))
+    actor = relationship('Actor', backref=backref('roles'))
+
+    def __repr__(self):
+        return '<role %r>' % (self.name)
+
+if __name__ == '__main__':
+    engine = create_engine('sqlite:///movie.db', echo=True)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    for movie in session.query(Movie).filter_by(
+        title='Hamlet'):
+        print movie
