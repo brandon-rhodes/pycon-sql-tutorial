3309f9d700b3470fec3678307dfaca9be1bce79e
Brandon Rhodes <brandon@rhodesmill.org>
2012-03-10 18:19:19 -0500

    Learned how to create both a case sensitive and insensitive movie title index.

--- all_indexes.sql
+++ all_indexes.sql
@@ -1,6 +1,7 @@
 -- Create indexes that support searches and joins from all directions.
 
-create index idx_movie_title on movie(title);
+create index idx_movie_title_binary on movie(title COLLATE BINARY);
+create index idx_movie_title_nocase on movie(title COLLATE NOCASE);
 create index idx_movie_year on movie(year);
 
 create index idx_actor_name on actor(name);
