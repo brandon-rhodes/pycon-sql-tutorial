5cc155981e523fa2541a07cc18cf7d6ec92ef468
Bill Tucker <imbilltucker@gmail.com>
2012-03-13 12:02:07 -0600

    add python sql koan structure; add pyc to ignore file

---
+++ python_sql_koans/python2/koans/about_having_sa.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from runner.koan import *
+
+class AboutHavingInSqlAlchemy(Koan):
+
+    def test_assert_truth(self) :
+        """
+        We shall contemplate the truth about selects..
+        """
+        self.assertTrue(___)  #this should be true
+    
+
+
